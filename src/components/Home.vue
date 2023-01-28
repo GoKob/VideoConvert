c5e270909b12aaed52575f323ee5544fbbdad94e
EC2 Default User <ec2-user@ip-172-31-44-26.ap-northeast-1.compute.internal>
2023-01-28 16:09:20 +0000

    fix home vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,38 +1,42 @@
 <template>
   <div class="home">
-    <img src="https://www.tv-asahi.co.jp/music/common-img/mv-new.jpg" />
+    <div class="container">
+        <div class="row">
+          <video id="video-player" class="mt-3 col-lg-6 col-12"
+            autoPlay
+            controls
+            playsinline
+          />
+          
+          <div class="col-lg-6">
+            <div class="row">
+              <div class="input-group mb-3 mt-3 col-12">
+                <input type="text" v-model="date" class="form-control" placeholder="Please set datetime" aria-label="Load" aria-describedby="Load" required>
+                <button class="btn btn-secondary" type="button" id="Load" v-on:click="setplayer">Load</button>
+              </div>
+              
+              <div class="input-group mb-3 col-12">
+                <input type="text" v-model="start" class="form-control" placeholder="StartPoint" aria-label="StartPoint" aria-describedby="StartTime" readonly required>
+                <button class="btn btn-primary" type="button" id="start" v-on:click="getCurrentTime">Start</button>
+              </div>
+              
+              <div class="input-group mb-3 col-12">
+                <input type="text" v-model="end" class="form-control" placeholder="EndPoint" aria-label="EndPoint" aria-describedby="EndTime" readonly required>
+                <button class="btn btn-success" type="button" id="end" v-on:click="getCurrentTime">End</button>
+              </div>
+              
+              <div class="input-group mb-3 col-12">
+                <input type="text" v-model="folder" class="form-control" placeholder="upload folder name" aria-label="folder" aria-describedby="folder" required>
+              </div>
+            </div>
+          </div>
+        </div>
 
-    <div class="input-group mb-3">
-      <input type="text" v-model="url" class="form-control" placeholder="Please paste URL" aria-label="Load" aria-describedby="Load">
-      <button class="btn btn-secondary" type="button" id="Load" v-on:click="setplayer">Load</button>
-    </div>
+        <div class="d-grid mt-4 mb-4">
+          <button v-bind:class="[activateSubmit == true ? 'btn btn-outline-dark': 'btn btn-dark']" v-bind:disabled="activateSubmit" type="button" data-bs-toggle="modal" data-bs-target="#Modal" v-on:click="requestConvert" >Convert</button>
+        </div>
+      </div>
 
-    <div class="col-8 mb-3">
-      <video id="video-player" 
-        autoPlay
-        controls
-        playsinline
-      />
-    </div>
-  
-    <div class="input-group mb-3">
-      <input type="text" v-model="start" class="form-control" placeholder="StartPoint" aria-label="StartPoint" aria-describedby="StartTime" readonly>
-      <button class="btn btn-primary" type="button" id="start" v-on:click="getCurrentTime">Start</button>
-    </div>
-    
-    <div class="input-group mb-3">
-      <input type="text" v-model="end" class="form-control" placeholder="EndPoint" aria-label="EndPoint" aria-describedby="EndTime" readonly>
-      <button class="btn btn-success" type="button" id="end" v-on:click="getCurrentTime">End</button>
-    </div>
-    
-    <div class="input-group mb-3">
-      <input type="text" v-model="folder" class="form-control" placeholder="upload folder name" aria-label="folder" aria-describedby="folder">
-    </div>
-    
-    <div class="d-grid gap-2 col-6 mx-auto">
-      <button class="btn btn-dark" type="button" data-bs-toggle="modal" data-bs-target="#Modal" v-on:click="requestConvert">Convert</button>
-    </div>
-    
     <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -68,23 +72,36 @@ export default {
   },
   data(){
     return{
-      url: null,
+      url: "https://drllcokaf794g.cloudfront.net/out/v1/d544d824eb784192aa931fe5334aa556/master.m3u8",
+      date: null,
       start: null,
       end: null,
       folder: null
     }
   },
+  computed: {
+    activateSubmit() {
+      if (this.start == null) {
+        return true;
+      } else if (this.end == null ) {
+        return true;
+      } else if (this.folder == null || this.folder == "") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
   methods: {
     setplayer: function() {
       player.attachHTMLVideoElement(document.getElementById('video-player'));
-      player.load(this.url);
+      player.load(this.url + "?start=" + this.date);
       player.play();
     },
     getCurrentTime: function(e) {
       console.log(player.core.state.position);
     
-      const query = this.url.split(/\?|&/).slice(1);
-      const target = new Date(query[0].split("start=")[1]);
+      const target = new Date(this.date);
       target.setSeconds(target.getSeconds() + parseInt(player.core.state.position, 10));
       this[e.target.id] = this.toISOString(target);
       
